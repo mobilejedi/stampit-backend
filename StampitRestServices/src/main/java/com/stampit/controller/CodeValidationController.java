4d0705445c74844522cec098d605f2447e7547f5
Emilio <emilio.sellitto@icloud.com>
2014-09-17 10:04:29 +0200

    Indentation

--- StampitRestServices/src/main/java/com/stampit/controller/CodeValidationController.java
+++ StampitRestServices/src/main/java/com/stampit/controller/CodeValidationController.java
@@ -40,99 +40,106 @@ import com.stampit.repositories.LogRepository;
 import com.stampit.repositories.MerchantRepository;
 
 @RestController
-public class CodeValidationController {	
-	
-	private static final Logger logger = LoggerFactory.getLogger(CodeValidationController.class);
-	
-	@Autowired private CodeRepository codeRepository;
-	@Autowired private ActiveCardRepository activeCardRepository;
-	@Autowired private CustomerRepository customerRepository;
-	@Autowired private BonusRepository bonusRepository;
-	@Autowired private AchievedBonusRepository achievedBonusRepository;
-	@Autowired private LogRepository logRepository;	
-	@Autowired private MerchantRepository merchantRepository;
-	
-	@RequestMapping("/validateCode")
-	public ResponseEntity<String> validateCodeAssignBonus(@RequestParam(value="idCustomer", required=true)Long idCustomer,  @RequestParam(value="code", required=true)String code, HttpServletRequest request) {		
-		String appUrl = String.format("%s://%s:%d/%s",request.getScheme(),  request.getServerName(), request.getServerPort(), request.getContextPath());		
-		Code codeObject = codeRepository.findByCode(code);
-		if(codeObject == null) {
-			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
-		}
-		Card codeCard = codeObject.getCard();
-		Customer codeCustomer = customerRepository.findOne(idCustomer);
-		ActiveCard codeActiveCard = activeCardRepository.findByCardAndCustomerAndStampsNumberNot(codeCard, codeCustomer, codeCard.getSlotsNum());
-		int stampsToAdd = codeObject.getStampParameters().getStampsNum();
-		if(codeActiveCard == null || codeActiveCard.getStampsNumber() == codeCard.getSlotsNum()) {
-			codeActiveCard = new ActiveCard();
-			codeActiveCard.setCard(codeCard);			
-			codeActiveCard.setCustomer(codeCustomer);					
-		}
-		int updatedStampsNumber = codeActiveCard.getStampsNumber() + stampsToAdd;
-		ResponseEntity<String> response = null;
-		String codeActiveCardUrl = appUrl + "/rest/activeCards/{idCard}";
-		if(updatedStampsNumber >= codeCard.getSlotsNum()) {
-			ActiveCard codeNewActiveCard = new ActiveCard();
-			codeNewActiveCard.setCard(codeCard);			
-			codeNewActiveCard.setCustomer(codeCustomer);
-			codeNewActiveCard.setStampsNumber(updatedStampsNumber - codeCard.getSlotsNum());
-			codeActiveCard.setStampsNumber(codeCard.getSlotsNum());
-			activeCardRepository.save(codeActiveCard);
-			activeCardRepository.save(codeNewActiveCard);
-			RestTemplate restTemplate = new RestTemplate();			
-			String content= restTemplate.getForObject(codeActiveCardUrl, String.class, codeNewActiveCard.getIdActiveCard());		
-			response =  new ResponseEntity<String>(content, new HttpHeaders(), HttpStatus.OK);
-			this.updateAchievedBonuses(codeActiveCard);
-			this.updateLogs(codeActiveCard, stampsToAdd);	
-			this.updateAchievedBonuses(codeNewActiveCard);
-			this.updateLogs(codeNewActiveCard, stampsToAdd);
-			return response;
-		} else {
-			codeActiveCard.setStampsNumber(updatedStampsNumber);
-			activeCardRepository.save(codeActiveCard);		
-			RestTemplate restTemplate = new RestTemplate();
-			String content= restTemplate.getForObject(codeActiveCardUrl, String.class, codeActiveCard.getIdActiveCard());		
-			response =  new ResponseEntity<String>(content, new HttpHeaders(), HttpStatus.OK);
-			this.updateAchievedBonuses(codeActiveCard);
-			this.updateLogs(codeActiveCard, stampsToAdd);
-			return response;
-		}
-		
-	}
-	
-	private void updateAchievedBonuses(ActiveCard activeCard) {
-		List<Bonus> bonusList = bonusRepository.findByCardOrderBySlotPosAsc(activeCard.getCard());
-		logger.info("bonusList size: " + bonusList.size());
-		List<AchievedBonus> achievedBonusList = achievedBonusRepository.findByActiveCard(activeCard);
-		for(Bonus currentBonus: bonusList) {
-			boolean alreadyPresent = false;
-			for(AchievedBonus currentAchievedBonus: achievedBonusList) {				
-				if(currentBonus.getIdBonus() == currentAchievedBonus.getBonus().getIdBonus()) {
-					alreadyPresent = true;
-					break;
-				}				
-			}
-			if(!alreadyPresent) {
-				if(activeCard.getStampsNumber() >= currentBonus.getSlotPos()) {
-					logger.info("activeCard stamps number: " + activeCard.getStampsNumber());
-					logger.info("currentBonus slots number: " + currentBonus.getSlotPos());
-					AchievedBonus newAchievedBonus = new AchievedBonus();
-					newAchievedBonus.setActiveCard(activeCard);
-					newAchievedBonus.setBonus(currentBonus);
-					achievedBonusRepository.save(newAchievedBonus);					
-				}
-			}
-		}
-	}
-	
-	private void updateLogs(ActiveCard activeCard, int addedStamps) {
-		Log newLog = new Log();
-		newLog.setActiveCard(activeCard);
-		newLog.setStampsNum(addedStamps);
-		newLog.setInsDate(new Date());
-		Merchant activeCardMerchant = merchantRepository.findMerchantByActiveCard(activeCard);
-		newLog.setLocation(activeCardMerchant.getGpsCoordinates());
-		logRepository.save(newLog);
-	}
+public class CodeValidationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CodeValidationController.class);
+
+    @Autowired
+    private CodeRepository codeRepository;
+    @Autowired
+    private ActiveCardRepository activeCardRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private BonusRepository bonusRepository;
+    @Autowired
+    private AchievedBonusRepository achievedBonusRepository;
+    @Autowired
+    private LogRepository logRepository;
+    @Autowired
+    private MerchantRepository merchantRepository;
+
+    @RequestMapping("/validateCode")
+    public ResponseEntity<String> validateCodeAssignBonus(@RequestParam(value = "idCustomer", required = true) Long idCustomer, @RequestParam(value = "code", required = true) String code, HttpServletRequest request) {
+        String appUrl = String.format("%s://%s:%d/%s", request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
+        Code codeObject = codeRepository.findByCode(code);
+        if (codeObject == null) {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
+        Card codeCard = codeObject.getCard();
+        Customer codeCustomer = customerRepository.findOne(idCustomer);
+        ActiveCard codeActiveCard = activeCardRepository.findByCardAndCustomerAndStampsNumberNot(codeCard, codeCustomer, codeCard.getSlotsNum());
+        int stampsToAdd = codeObject.getStampParameters().getStampsNum();
+        if (codeActiveCard == null || codeActiveCard.getStampsNumber() == codeCard.getSlotsNum()) {
+            codeActiveCard = new ActiveCard();
+            codeActiveCard.setCard(codeCard);
+            codeActiveCard.setCustomer(codeCustomer);
+        }
+        int updatedStampsNumber = codeActiveCard.getStampsNumber() + stampsToAdd;
+        ResponseEntity<String> response = null;
+        String codeActiveCardUrl = appUrl + "/rest/activeCards/{idCard}";
+        if (updatedStampsNumber >= codeCard.getSlotsNum()) {
+            ActiveCard codeNewActiveCard = new ActiveCard();
+            codeNewActiveCard.setCard(codeCard);
+            codeNewActiveCard.setCustomer(codeCustomer);
+            codeNewActiveCard.setStampsNumber(updatedStampsNumber - codeCard.getSlotsNum());
+            codeActiveCard.setStampsNumber(codeCard.getSlotsNum());
+            activeCardRepository.save(codeActiveCard);
+            activeCardRepository.save(codeNewActiveCard);
+            RestTemplate restTemplate = new RestTemplate();
+            String content = restTemplate.getForObject(codeActiveCardUrl, String.class, codeNewActiveCard.getIdActiveCard());
+            response = new ResponseEntity<String>(content, new HttpHeaders(), HttpStatus.OK);
+            this.updateAchievedBonuses(codeActiveCard);
+            this.updateLogs(codeActiveCard, stampsToAdd);
+            this.updateAchievedBonuses(codeNewActiveCard);
+            this.updateLogs(codeNewActiveCard, stampsToAdd);
+            return response;
+        } else {
+            codeActiveCard.setStampsNumber(updatedStampsNumber);
+            activeCardRepository.save(codeActiveCard);
+            RestTemplate restTemplate = new RestTemplate();
+            String content = restTemplate.getForObject(codeActiveCardUrl, String.class, codeActiveCard.getIdActiveCard());
+            response = new ResponseEntity<String>(content, new HttpHeaders(), HttpStatus.OK);
+            this.updateAchievedBonuses(codeActiveCard);
+            this.updateLogs(codeActiveCard, stampsToAdd);
+            return response;
+        }
+
+    }
+
+    private void updateAchievedBonuses(ActiveCard activeCard) {
+        List<Bonus> bonusList = bonusRepository.findByCardOrderBySlotPosAsc(activeCard.getCard());
+        logger.info("bonusList size: " + bonusList.size());
+        List<AchievedBonus> achievedBonusList = achievedBonusRepository.findByActiveCard(activeCard);
+        for (Bonus currentBonus : bonusList) {
+            boolean alreadyPresent = false;
+            for (AchievedBonus currentAchievedBonus : achievedBonusList) {
+                if (currentBonus.getIdBonus() == currentAchievedBonus.getBonus().getIdBonus()) {
+                    alreadyPresent = true;
+                    break;
+                }
+            }
+            if (!alreadyPresent) {
+                if (activeCard.getStampsNumber() >= currentBonus.getSlotPos()) {
+                    logger.info("activeCard stamps number: " + activeCard.getStampsNumber());
+                    logger.info("currentBonus slots number: " + currentBonus.getSlotPos());
+                    AchievedBonus newAchievedBonus = new AchievedBonus();
+                    newAchievedBonus.setActiveCard(activeCard);
+                    newAchievedBonus.setBonus(currentBonus);
+                    achievedBonusRepository.save(newAchievedBonus);
+                }
+            }
+        }
+    }
+
+    private void updateLogs(ActiveCard activeCard, int addedStamps) {
+        Log newLog = new Log();
+        newLog.setActiveCard(activeCard);
+        newLog.setStampsNum(addedStamps);
+        newLog.setInsDate(new Date());
+        Merchant activeCardMerchant = merchantRepository.findMerchantByActiveCard(activeCard);
+        newLog.setLocation(activeCardMerchant.getGpsCoordinates());
+        logRepository.save(newLog);
+    }
 
 }
